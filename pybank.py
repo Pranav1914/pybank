7db2a5f899da5485ee351ea57c3f3f3eca10b009
Pranav Angadi <pranavangadi99@gmail.com>
2026-03-12 19:29:41 +0530

    final draft

--- pybank.py
+++ pybank.py
@@ -24,7 +24,33 @@ def login():
         log_pin=int(input("Enter pin: "))
         if result2[0]==log_pin:
             print("Login successfull")
-            
+            print("1.check balance 2.deposit 3.withdraw 0.logout\n")
+            op2=int(input("Enter option: "))
+            if op2==1:
+                cur3=mydb.cursor()
+                q3="select balance from bank where acc_no=%s"
+                cur3.execute(q3,(log_accno,))
+                result3=cur3.fetchone()
+                print("Current balance: ",result3[0])
+            elif op2==2:
+                deposit_amount=int(input("Enter amount to deposit: "))
+                cur3=mydb.cursor()
+                q3="update bank set balance=balance+%s where acc_no=%s"
+                cur3.execute(q3,(deposit_amount,log_accno))
+                mydb.commit()
+            elif op2==3:
+                withdraw_amount=int(input("Enter amount to withdraw: "))
+                cur3=mydb.cursor()
+                q3="select balance from bank where acc_no=%s"
+                cur3.execute(q3,(log_accno,))
+                result3=cur3.fetchone()
+                if result3[0]>=withdraw_amount:
+                    q4="update bank set balance=balance-%s where acc_no=%s"
+                    cur3.execute(q4,(withdraw_amount,log_accno))
+                    mydb.commit()
+                else:
+                    print("Insufficient balance")
+                
         else:
             print("Login fail! incorrect pin")
     else:
